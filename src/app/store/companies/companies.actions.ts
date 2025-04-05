61f7275c5c890e3a133fe630021cd1579d911873
NisanEduardo <eduardoleitefrontend@gmail.com>
2025-04-05 20:23:23 -0300

    feat: create action and reducer for current selected company

--- src/app/store/companies/companies.actions.ts
+++ src/app/store/companies/companies.actions.ts
@@ -5,3 +5,8 @@ export const setCompanies = createAction(
 	"[Companies] Set loaded companies",
 	(companies: ICompany[]) => ({ companies }),
 );
+
+export const setCurrentSelectedCompany = createAction(
+	"[Company] Set current selected company",
+	(company: ICompany) => ({ company }),
+);
--- src/app/store/companies/companies.reducer.ts
+++ src/app/store/companies/companies.reducer.ts
@@ -1,13 +1,15 @@
 import { createReducer, on } from "@ngrx/store";
-import { setCompanies } from "./companies.actions";
+import { setCompanies, setCurrentSelectedCompany } from "./companies.actions";
 import type { ICompany } from "../../interfaces/companies.interface";
 
 export interface ICompaniesState {
 	companies: ICompany[];
+	currentSelectedCompany: ICompany | null;
 }
 
 export const initialState: ICompaniesState = {
 	companies: [],
+	currentSelectedCompany: null,
 };
 
 export const companiesReducer = createReducer(
@@ -15,5 +17,9 @@ export const companiesReducer = createReducer(
 	on(setCompanies, (state, action) => ({
 		...state,
 		companies: action.companies,
+	})),
+	on(setCurrentSelectedCompany, (state, action) => ({
+		...state,
+		currentSelectedCompany: action.company,
 	})),
 );
