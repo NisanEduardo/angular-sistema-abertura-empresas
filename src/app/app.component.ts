19e77ccbc1b708f7b7545f77de3a20e801af2c17
EduardoLeiteN <eduardoleitefrontend@gmail.com>
2025-04-14 20:07:51 -0300

    feat: create companies CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,62 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { AsyncPipe } from "@angular/common";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 
-import { Observable } from "rxjs";
-import { Store } from "@ngrx/store";
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 // biome-ignore lint/style/useImportType: <explanation>
-import { CompaniesService } from "./services/companies/companies.service";
-import type { ICompaniesState } from "./store/companies/companies.reducer";
+import { CompaniesService } from './services/companies/companies.service';
+import type { ICompaniesState } from './store/companies/companies.reducer';
 import {
-	setCompanies,
-	setCurrentSelectedCompany,
-} from "./store/companies/companies.actions";
+  setCompanies,
+  setCurrentSelectedCompany,
+} from './store/companies/companies.actions';
 
 import {
-	selectCompaniesStore,
-	selectCurrentCompanyStore,
-} from "./store/companies/companies.selectors";
+  selectCompaniesStore,
+  selectCurrentCompanyStore,
+} from './store/companies/companies.selectors';
 
-import type { ICompany } from "./interfaces/companies.interface";
-import { CurrentCompanyComponent } from "./components/current-company/current-company.component";
-import { CompanyFormComponent } from "./components/company-form/company-form.component";
+import type { ICompany } from './interfaces/companies.interface';
+import { CurrentCompanyComponent } from './components/current-company/current-company.component';
+import { CompanyFormComponent } from './components/company-form/company-form.component';
 
 @Component({
-	selector: "app-root",
-	standalone: true,
-	imports: [
-		RouterOutlet,
-		AsyncPipe,
-		CurrentCompanyComponent,
-		CompanyFormComponent,
-	],
-	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.scss",
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    CurrentCompanyComponent,
+    CompanyFormComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-	companies$: Observable<ICompany[]>;
-	currentSelectedCompany$: Observable<ICompany>;
+  companies$: Observable<ICompany[]>;
+  currentSelectedCompany$: Observable<ICompany>;
 
-	constructor(
-		private companiesService: CompaniesService,
-		private store: Store<{ companiesStore: ICompaniesState }>,
-	) {
-		this.companies$ = this.store.select(selectCompaniesStore);
-		this.currentSelectedCompany$ = this.store.select(selectCurrentCompanyStore);
-	}
+  constructor(
+    private companiesService: CompaniesService,
+    private store: Store<{ companiesStore: ICompaniesState }>
+  ) {
+    this.companies$ = this.store.select(selectCompaniesStore);
+    this.currentSelectedCompany$ = this.store.select(selectCurrentCompanyStore);
+  }
 
-	ngOnInit(): void {
-		this.getCompanies();
-	}
+  ngOnInit(): void {
+    this.getCompanies();
+  }
 
-	getCompanies() {
-		return this.companiesService.getCompanies().subscribe((response) => {
-			this.store.dispatch(setCompanies(response));
-		});
-	}
+  getCompanies() {
+    return this.companiesService.listCompanies().subscribe((response) => {
+      this.store.dispatch(setCompanies(response));
+    });
+  }
 
-	onViewClick(company: ICompany) {
-		this.store.dispatch(setCurrentSelectedCompany(company));
-	}
+  onViewClick(company: ICompany) {
+    this.store.dispatch(setCurrentSelectedCompany(company));
+  }
 }
--- src/app/services/companies/companies.service.ts
+++ src/app/services/companies/companies.service.ts
@@ -1,18 +1,34 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import type { ICompany } from "../../interfaces/companies.interface";
-import { HttpClient } from "@angular/common/http";
+import type { ICompany } from '../../interfaces/companies.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-	providedIn: "root",
+  providedIn: 'root',
 })
 export class CompaniesService {
-	private apiUrl = "http://localhost:3000";
+  private apiUrl = 'http://localhost:3000';
 
-	constructor(private api: HttpClient) {}
+  constructor(private api: HttpClient) {}
 
-	getCompanies(): Observable<ICompany[]> {
-		return this.api.get<ICompany[]>(`${this.apiUrl}/empresas`);
-	}
+  listCompanies(): Observable<ICompany[]> {
+    return this.api.get<ICompany[]>(`${this.apiUrl}/empresas`);
+  }
+
+  createCompany(company: ICompany) {
+    return this.api.post(`${this.apiUrl}/empresas`, {
+      company,
+    });
+  }
+
+  updateCompany(company: ICompany) {
+    return this.api.put(`${this.apiUrl}/empresas/${company.id}`, {
+      company,
+    });
+  }
+
+  deleteCompany(companyId: string) {
+    return this.api.delete(`${this.apiUrl}/empresas/${companyId}`);
+  }
 }
