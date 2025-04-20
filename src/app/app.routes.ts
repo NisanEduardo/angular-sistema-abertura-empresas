d10d3a4d70702f3476c8ae0ae91c14ae6d0f6297
NisanEduardo <eduardoleitefrontend@gmail.com>
2025-04-19 22:50:33 -0300

    feat: adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { CompaniesListComponent } from "./components/companies/companies-list/companies-list.component";
+import { CompanyFormComponent } from "./components/company-form/company-form.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", component: CompaniesListComponent },
+	{ path: "registrar-empresa", component: CompanyFormComponent },
+];
